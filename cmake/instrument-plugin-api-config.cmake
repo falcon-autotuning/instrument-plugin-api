ea445403430f729e9d5c53dde3fbae99f58e410e
tjk144 <tkovach@wisc.edu>
2026-06-01 16:47:58 -0500

    Added helper code for constructing plugins

--- cmake/instrument-plugin-api-config.cmake
+++ cmake/instrument-plugin-api-config.cmake
@@ -1,1 +1,44 @@
 include("${CMAKE_CURRENT_LIST_DIR}/instrument-plugin-api-targets.cmake")
+
+function(add_instrument_plugin TARGET_NAME)
+  set(options "")
+  set(oneValueArgs "")
+  set(multiValueArgs SOURCES LINK_LIBRARIES INCLUDE_DIRS)
+
+  cmake_parse_arguments(PLUGIN "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  if(NOT PLUGIN_SOURCES)
+    message(FATAL_ERROR "add_instrument_plugin: SOURCES argument is required")
+  endif()
+
+  # Create a MODULE library (dlopen/LoadLibrary)
+  add_library(${TARGET_NAME} MODULE ${PLUGIN_SOURCES})
+
+  # Ensure no "lib" prefix
+  set_target_properties(${TARGET_NAME}
+    PROPERTIES
+      PREFIX ""
+      POSITION_INDEPENDENT_CODE ON
+  )
+
+  # Include directories
+  target_include_directories(${TARGET_NAME}
+    PRIVATE
+      ${PLUGIN_INCLUDE_DIRS}
+  )
+
+  # Link plugin API
+  target_link_libraries(${TARGET_NAME}
+    PRIVATE
+      instrument-plugin-api::instrument-plugin-api
+      ${PLUGIN_LINK_LIBRARIES}
+  )
+
+  # Ensure export macro is set correctly for Windows
+  target_compile_definitions(${TARGET_NAME}
+    PRIVATE INSTRUMENT_PLUGIN_BUILD
+  )
+
+  message(STATUS "Configured instrument plugin: ${TARGET_NAME}")
+endfunction()
